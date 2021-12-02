1ff9a948b4a0d2324a1e7a8ac4c314fb07098628
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-12-02 21:26:37 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../style/App.css'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{background:"var(--darkBlue)"}}>test</h1>
+        <i className="fa fa-home"></i>
+      </div>
+    )
+  }
+}
+
+export default App
