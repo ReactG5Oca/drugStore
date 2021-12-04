c0060437cb230c257c46b03983e1dbf7e3661917
AyaMeizer <90200721+AyaMeizer@users.noreply.github.com>
2021-12-04 13:02:46 +0200

    enhanced-NavToggle-CheckoutObject-footer-Style

--- src/components/CheckoutComponent.js
+++ src/components/CheckoutComponent.js
@@ -4,15 +4,41 @@ import PlacedOrder from "./PlacedOrder";
 import "../style/checkout.css"
 export default class Checkout extends Component {
     state={
-        submitState:""
+        submitState:"",
+        username:"",
+        phone:"",
+        msgErr:"",
+        msgErr2:""
     }
-    submitted=()=>{
-        this.setState({submitState:"submitted"})
+    submitted=(e)=>{
+        e.preventDefault();
+        if(this.state.submitState==="ok"){
+            this.setState({submitState:"submitted"})
+        }
+    }
+    validation=(event,username)=>{
+        let i=0;
+        this.setState({[username]:event.target.value})
+         if(this.state.username.length<5){
+            this.setState({msgErr:"Username should be larger than 6 digits"})
+            i++
+        }
+         if(this.state.username.length>=5){
+            this.setState({msgErr:""})
+        }
+            if(this.state.phone.length!==9){
+            this.setState({msgErr2:"Phone Number should be less than 10 digits"})
+            i++
+        }
+         if(this.state.phone.length===9){
+            this.setState({msgErr2:""})
+        }
+         if(i===0){
+            this.setState({submitState:"ok"})
+        }
     }
     render(){
         let data=[
-            {   total:"JOD 69",
-                taxes:"JOD 0"},
             {
                 productName:"Drug 1",
                 price:"JOD 20",
@@ -28,7 +54,9 @@ export default class Checkout extends Component {
             {
                 productName:"Drug 4",
                 price:"JOD 30",
-            }
+            },
+              "JOD 0",
+              "JOD 69",
         ]
         let values=data.map((item,key)=>(
            <BillCards price={item.price} productName={item.productName} totalPrice={item.total} key={key} />
@@ -40,19 +68,21 @@ export default class Checkout extends Component {
             <div>
             <form className="form" onSubmit={this.submitted}>
                 <div className="checkCont">
-                <div>
+                <div className="formSection">
                 <h3 className="formTitle">Please fill your information</h3>
                <div>
                    <span>Personal Information:</span>
-                <input type="text" placeholder="User Name" className="formInputs userName" required></input>
+               <input onChange={(e)=>this.validation(e,"username")} value={this.state.username} type="text" placeholder="User Name" className="formInputs userName" required></input>
+               <h6 id="msgErr">{this.state.msgErr}</h6>
                <input type="email" placeholder="Email" className="formInputs email" required></input>
-               <input type="tel" placeholder="Phone Number" className="formInputs phoneNumber" required></input>
+               <input onChange={(e)=>this.validation(e,"phone")} value={this.state.phone} type="tel" placeholder="Phone Number" className="formInputs phoneNumber" required></input>
+               <h6 id="msgErr2">{this.state.msgErr2}</h6>
                </div><br/>
                <div>
                <span>Address Information:</span>
                <input type="text" placeholder="Country" className="formInputs address" required></input>
                <input type="text" placeholder="City" className="formInputs address" required></input>
-               <input type="text" placeholder="Address 2" className="formInputs address" required></input>
+               <input type="text" placeholder="Address 2 / optional" className="formInputs address"></input>
                </div>
                <div>
                    <br/>
@@ -70,12 +100,12 @@ export default class Checkout extends Component {
                 <h3 className="billTitle">Your Order</h3>
                  <span className="products"> {values} </span><hr/>
                   <span className="totalTaxes">purchase
-                  <span>{data[0].total}</span></span>
+                  <span>{data[data.length-1]}</span></span>
                   <span className="totalTaxes">Taxes
-                  <span>{data[0].taxes}</span></span>
+                  <span>{data[data.length-2]}</span></span>
                   <hr/>
                   <div className="totalSection"><span>Total:  </span>
-                  <span className="totalPrice"> {data[0].total}</span>
+                  <span className="totalPrice"> {data[data.length-1]}</span>
                   </div>
                   </div>
                   </div>
