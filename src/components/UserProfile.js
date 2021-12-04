ad7bdd31cdb9f0518d80dbefd8c9a146423b5a27
RaniaAbualnadi <rania.abunnadi@gmail.com>
2021-12-04 16:46:42 +0300

    UserProfileFunctionality

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,23 +3,29 @@ import "../style/UserProfile.css";
 import Weather from "./Weather";
 
 class UserProfile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
+    const users = JSON.parse(localStorage.getItem("users"));
+
     return (
       <>
         <Weather />
 
-        <h1 className="UserProfileh1">UserProfile</h1>
+        <h1 className="UserProfileh1">User Profile</h1>
 
         <div className="image-information-containerUser">
           <div className="image-containerUser">
             <img src="../assets/images/Default.jfif" alt="Default_Image" />
           </div>
           <div className="information-containerUser">
-            <h1>information</h1>
-            <p>First Name :haneen </p>
-            <p>Last Name:edinat </p>
-            <p>Phone number:088888888888</p>
-            <p>Email:haneneeee@hga;dkkf</p>
+            <h3>Personal information</h3>
+            <p>First Name :{users.username} </p>
+            {/* <p>Last Name:edinat </p>
+            <p>Phone number:088888888888</p> */}
+            <p>Email:{users.email}</p>
           </div>
         </div>
       </>
