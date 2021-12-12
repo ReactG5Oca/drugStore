17627e8009db365ac46e4a5ef26f95f146a55fd5
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-12-12 15:16:37 +0200

    add to cart toast

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,40 +1,60 @@
 import React, { Component } from "react";
+import { Snackbar, Alert } from "@mui/material";
 
 export class AddToCart extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      open: false,
+    };
   }
   onClickHandler = () => {
-    
     const cartArray = localStorage.getItem("cartKey")
       ? JSON.parse(localStorage.getItem("cartKey"))
       : [];
 
     const cartAddArry = this.props.itemId;
-    let exist =false
-    cartArray.forEach(element => {
-      if (element.id===cartAddArry.id) {
-        exist=true
+    let exist = false;
+    cartArray.forEach((element) => {
+      if (element.id === cartAddArry.id) {
+        exist = true;
       }
     });
     if (!exist) {
       cartArray.push(cartAddArry);
+      this.handleClick();
     }
     localStorage.setItem("cartKey", JSON.stringify(cartArray));
-
+  };
+  handleClick = () => {
+    this.setState((state) => ({ open: !this.state.open }));
+  };
+  handleClose = () => {
+    this.setState((state) => ({ open: false }));
   };
   render() {
     return (
-      <button
-        className="add-to-cart-btn"
-        onClick={this.onClickHandler}
-      >
-        <div className="btn-icon">
-          <i className="fa-solid fa-cart-arrow-down"></i>
-        </div>
-        <div className="btn-text"> Add to Cart</div>
-      </button>
+      <>
+        <button className="add-to-cart-btn" onClick={this.onClickHandler}>
+          <div className="btn-icon">
+            <i className="fa-solid fa-cart-arrow-down"></i>
+          </div>
+          <div className="btn-text"> Add to Cart</div>
+        </button>
+        <Snackbar
+          open={this.state.open}
+          autoHideDuration={3000}
+          onClose={this.handleClose}
+        >
+          <Alert
+            onClose={this.handleClose}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            Added Successfully!
+          </Alert>
+        </Snackbar>
+      </>
     );
   }
 }
