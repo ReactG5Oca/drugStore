d970f1d4745d862da1f13bebed8df78ee9c16250
RaniaAbualnadi <rania.abunnadi@gmail.com>
2021-12-12 14:46:50 +0300

    Change the color of AddToCart

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,21 +6,20 @@ class Card extends Component {
   render() {
     return (
       <div>
-      <div className="Card">
-        <div className="image-containerCard">
-          <img src={this.props.src} alt={this.props.name} />
-        </div>
-        <div className="information-containerCard">
-         <div className="card-title">{this.props.title}</div>
-          <div className="card-brief-description">
-            <p>{this.props.description}</p>
+        <div className="Card">
+          <div className="image-containerCard">
+            <img src={this.props.src} alt={this.props.name} />
           </div>
-          <div className="PriceCard"> Price :{this.props.price}</div>
-          <AddToCart itemId={this.props} />
+          <div className="information-containerCard">
+            <div className="card-title">{this.props.title}</div>
+            <div className="card-brief-description">
+              <p>{this.props.description}</p>
+            </div>
+            <div className="PriceCard"> {this.props.price} JOD</div>
+            <AddToCart itemId={this.props} />
+          </div>
         </div>
       </div>
-      </div>
-
     );
   }
 }
